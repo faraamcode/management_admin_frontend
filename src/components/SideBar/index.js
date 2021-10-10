e328d3facac757631904c7795e96651e8b7a9767
faraamcode <abdulrasaqalagbede@gmail.com>
2021-10-10 08:04:56 +0100

    feat(dashboard routing)

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -8,6 +8,20 @@ import { FcAbout } from 'react-icons/fc'
 import { Transition } from '../../common/Global.Style'
 import { Dark, Grey, White } from '../../utils/color'
 import { logRoles } from '@testing-library/react'
+import {
+  DASHBOARD_ABOUT_URL,
+  DASHBOARD_ADMISSION_URL,
+  DASHBOARD_CALENDAR_URL,
+  DASHBOARD_CONTACT_URL,
+  DASHBOARD_EVENTS_URL,
+  DASHBOARD_GALLERY_URL,
+  DASHBOARD_HERO_URL,
+  DASHBOARD_MISSION_URL,
+  DASHBOARD_NEWS_URL,
+  DASHBOARD_STUDENTS_URL,
+  DASHBOARD_URL,
+  DASHBOARD_USER_URL
+} from '../../common/route'
 const Wrapper = styled.div`
   //   margin-top: 10px;
   width: 300px;
@@ -82,11 +96,71 @@ function SideBar () {
   return (
     <Wrapper click={click} onClick={() => setClick(!click)}>
       <MenuContainerStyle>
-        <MenuItem title='Dashboard' url='/' logo={<FaAddressBook />} />
-        <MenuItem title='Hero Section' url='/' logo={<MdDomain />} />
-        <MenuItem title='About School' url='/' logo={<FcAbout />} />
-        <MenuItem title='School Events' url='/' logo={<MdEventAvailable />} />
-        <MenuItem title='School News' url='/' logo={<FaRegNewspaper />} />
+        <MenuItem
+          title='Dashboard'
+          url={DASHBOARD_URL}
+          logo={<FaAddressBook />}
+        />
+        <MenuItem
+          title='Hero Section'
+          url={DASHBOARD_HERO_URL}
+          logo={<MdDomain />}
+        />
+        <MenuItem
+          title='About School'
+          url={DASHBOARD_ABOUT_URL}
+          logo={<FcAbout />}
+        />
+        <MenuItem
+          title='School Events'
+          url={DASHBOARD_EVENTS_URL}
+          logo={<MdEventAvailable />}
+        />
+        <MenuItem
+          title='CONTACTS'
+          url={DASHBOARD_CONTACT_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='Admission'
+          url={DASHBOARD_ADMISSION_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='Calendar'
+          url={DASHBOARD_CALENDAR_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='Gallery'
+          url={DASHBOARD_GALLERY_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='Mission'
+          url={DASHBOARD_MISSION_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='News'
+          url={DASHBOARD_NEWS_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='User'
+          url={DASHBOARD_USER_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='Student Perspectives'
+          url={DASHBOARD_STUDENTS_URL}
+          logo={<FaRegNewspaper />}
+        />
+        <MenuItem
+          title='School News'
+          url={DASHBOARD_NEWS_URL}
+          logo={<FaRegNewspaper />}
+        />
       </MenuContainerStyle>
     </Wrapper>
   )
